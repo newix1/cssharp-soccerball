1e22c91a4b5d90a8cda316d2bee0368759e5973f
newix1 <123808307+newix1@users.noreply.github.com>
2023-12-26 13:19:29 +0500

    Add files via upload

---
+++ Soccerball.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace Soccerball;
+public class Soccerball : BasePlugin
+{
+    public override string ModuleName => "Soccerball by Newix";
+
+    public override string ModuleVersion => "0.0.1";
+
+    [ConsoleCommand("sb", "Spawn soccerball")]
+    public void OnCommand(CCSPlayerController? player, CommandInfo command)
+    {
+        var entity = Utilities.CreateEntityByName<CPhysicsPropMultiplayer>("prop_physics_multiplayer");
+
+        if (entity == null || !entity.IsValid)
+        {
+            return;
+        }
+
+        entity.Teleport(
+            new Vector(
+                player.PlayerPawn.Value!.AbsOrigin!.X,
+                player.PlayerPawn.Value!.AbsOrigin!.Y,
+                player.PlayerPawn.Value!.AbsOrigin!.Z
+            ),
+            player.PlayerPawn.Value!.EyeAngles,
+            player.PlayerPawn.Value!.AbsVelocity
+        );
+
+        entity.SetModel("models/props/de_dust/hr_dust/dust_soccerball/dust_soccer_ball001.vmdl");
+
+        entity.Render = Color.Red;
+
+        entity.DispatchSpawn();
+    }
+
+}
